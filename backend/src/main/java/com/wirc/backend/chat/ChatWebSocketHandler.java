47e65da5541eb4a4a6f2959a86eeaa93cc8ee764
8030078-estg <8030078@estg.ipp.pt>
2026-03-01 20:03:13 +0000

    first commit

--- backend/src/main/java/com/wirc/backend/chat/ChatWebSocketHandler.java
+++ backend/src/main/java/com/wirc/backend/chat/ChatWebSocketHandler.java
@@ -1,12 +1,12 @@
 package com.wirc.backend.chat;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.Set;
@@ -28,7 +28,7 @@ public class ChatWebSocketHandler extends TextWebSocketHandler {
         ChatMessage chatMessage;
         try {
             chatMessage = objectMapper.readValue(message.getPayload(), ChatMessage.class);
-        } catch (JsonProcessingException ex) {
+        } catch (Exception ex) {
             session.sendMessage(new TextMessage("{\"user\":\"system\",\"message\":\"Mensagem inválida\"}"));
             return;
         }
