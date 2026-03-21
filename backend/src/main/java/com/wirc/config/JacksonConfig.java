8c9715d5882bab4646180c2aa581faedadbebbd1
fernandoeduardolucas <fernandoeduardolucas@icloud.com>
2026-03-21 18:56:11 +0000

    Fix websocket Instant serialization

--- backend/src/main/java/com/wirc/config/JacksonConfig.java
+++ backend/src/main/java/com/wirc/config/JacksonConfig.java
@@ -1,15 +1,15 @@
 package com.wirc.config;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+        return builder.build();
     }
 }
