8f4ba5dc5964167df24b40c1220dbf998681da81
8030078-estg <8030078@estg.ipp.pt>
2026-03-21 09:44:26 +0000

    first commit

--- backend/src/main/java/com/wirc/chat/ChatWebSocketHandler.java
+++ backend/src/main/java/com/wirc/chat/ChatWebSocketHandler.java
@@ -1,13 +1,14 @@
 package com.wirc.chat;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wirc.dto.ChatMessage;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-import tools.jackson.databind.ObjectMapper;
+
 
 
 import java.io.IOException;
